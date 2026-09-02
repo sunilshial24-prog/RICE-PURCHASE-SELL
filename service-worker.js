914753d38f1c94abdba9e790cc2d912947cf56af
sunilshial24-prog <sunilshial24@gmail.com>
2026-09-02 16:02:30 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE='rice-register-v6';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))})
